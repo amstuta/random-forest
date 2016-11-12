5f54e8e0cdadd803a10919de01ea06720e593d0e
amstut_a <arthur.amstutz@gmail.com>
2016-11-12 19:00:33 +0000

    almost pep8 compliant

--- python/RandomForest.py
+++ python/RandomForest.py
@@ -8,11 +8,7 @@ from DecisionTree import DecisionTreeClassifier
 
 
 """
-TODO: - add arguments to tree (max_depth, criterion)
-      - add same arguments to random forest
-      - implement a thread pool to fit multiple trees at the same time
-      - comment functions
-      - set code to PEP8 norm
+TODO: - implement a thread pool to fit multiple trees at the same time
       - unittests
 """
 
@@ -30,7 +26,6 @@ class RandomForestClassifier(object):
         self.nb_samples = nb_samples
         self.max_depth = max_depth
 
-
     """
     Trains self.nb_trees number of decision trees.
     :param  data:   A list of lists with the last element of each list being
@@ -46,7 +41,6 @@ class RandomForestClassifier(object):
 
             self.trees.append(tree)
 
-
     """
     Returns a prediction for the given feature. The result is the value that
     gets the most votes.
@@ -59,7 +53,6 @@ class RandomForestClassifier(object):
             predictions.append(tree.predict(feature))
 
         return max(set(predictions), key=predictions.count)
-
 
 
 def test_rf():
@@ -83,6 +76,6 @@ def test_rf():
     logging.info("Error rate: {}".format(errors / len(features) * 100))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     test_rf()
